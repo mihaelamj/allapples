d871fb3ac82ab3dfc136978b43d65d990ddf7384
Mihaela MJ <mihaelamj@me.com>
2020-11-27 21:54:56 +0100

    updated manifest

--- Package.swift
+++ Package.swift
@@ -4,28 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "AllApples",
+  name: "AllApples",
   platforms: [
-    .macOS(.v10_14), .iOS(.v12)
+    .macOS(.v10_14),
+    .iOS(.v12)
   ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AllApples",
-            targets: ["AllApples"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AllApples",
-            dependencies: []),
-        .testTarget(
-            name: "AllApplesTests",
-            dependencies: ["AllApples"]),
-    ]
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "AllApples",
+      targets: ["AllApples"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "AllApples",
+      dependencies: []),
+    .testTarget(
+      name: "AllApplesTests",
+      dependencies: ["AllApples"]),
+  ]
 )
